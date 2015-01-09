8fb52f27459a2f8c42a5a826659a84bdc4543694
Ken Wilcox <wilcoxk@gmail.com>
2015-01-09 16:12:07 -0700

    Added TAG

--- app/src/main/java/com/k3nx/mybank/BankAccount.java
+++ app/src/main/java/com/k3nx/mybank/BankAccount.java
@@ -1,20 +1,26 @@
 package com.k3nx.mybank;
 
+import android.util.Log;
+
 /**
  * Created by Ken Wilcox on 1/9/2015 2:55 PM.
  *
  */
 public class BankAccount {
 
+    private static final String TAG = "BankAccount";
+
     private double mBalance;
     public final static double OVERDRAFT_FEE = 30;
 
     public void withdraw(double amount) {
         mBalance -= amount;
+        Log.d(TAG, "Withdraw: " + amount);
     }
 
     public void deposit(double amount) {
         mBalance += amount;
+        Log.d(TAG, "Deposit: " + amount);
     }
 
     public double getBalance() {
