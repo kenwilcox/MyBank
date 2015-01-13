0e51576a84e0d896f5ea3de1f5faab6572db6580
Ken Wilcox <wilcoxk@gmail.com>
2015-01-13 16:09:29 -0700

    update comment

--- app/src/main/java/com/k3nx/mybank/CheckingAccount.java
+++ app/src/main/java/com/k3nx/mybank/CheckingAccount.java
@@ -1,7 +1,7 @@
 package com.k3nx.mybank;
 
 /**
- * Created by kwilcox on 1/13/2015 3:42 PM.
+ * Created by Ken Wilcox on 1/13/2015 3:42 PM.
  * Checking Account Class
  */
 public class CheckingAccount extends BankAccount {
--- app/src/main/java/com/k3nx/mybank/SavingsAccount.java
+++ app/src/main/java/com/k3nx/mybank/SavingsAccount.java
@@ -3,7 +3,7 @@ package com.k3nx.mybank;
 import android.util.Log;
 
 /**
- * Created by kwilcox on 1/13/2015 3:42 PM.
+ * Created by Ken Wilcox on 1/13/2015 3:42 PM.
  * Savings Account Class
  */
 public class SavingsAccount extends BankAccount {
