55846c42349360446c87a66e1c0e534846481be1
Ken Wilcox <wilcoxk@gmail.com>
2015-01-13 16:01:23 -0700

    override withdraw to limit the number

--- app/src/main/java/com/k3nx/mybank/SavingsAccount.java
+++ app/src/main/java/com/k3nx/mybank/SavingsAccount.java
@@ -1,8 +1,20 @@
 package com.k3nx.mybank;
 
+import android.util.Log;
+
 /**
  * Created by kwilcox on 1/13/2015 3:42 PM.
  * Savings Account Class
  */
 public class SavingsAccount extends BankAccount {
+    private static final String TAG = "SavingsAccount";
+
+    @Override
+    public void withdraw(double amount) {
+        Log.d(TAG, "Withdrawing from savings.");
+        if (super.numberOfWithdrawals() >= 3) {
+            return;
+        }
+        super.withdraw(amount);
+    }
 }
